4b6e640680a5556782b37805ddbfff632b09b97c
thenotoriousnewar <33809346+thenotoriousnewar@users.noreply.github.com>
2017-11-20 07:39:59 +0545

    Add files via upload

---
+++ greater than.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//int sum(int,int);
+int main()
+{
+int a,b,c;
+printf("enter a number:");
+scanf("%d",&a);
+printf("enter another number");
+scanf("%d",&b);
+c = sum(a,b);
+printf("The sum of %d and %d is %d",a,b,c);
+return 0;
+}
+int sum(int x, int y)
+{
+    int s;
+    s = x+y;
+    return s;
+}
